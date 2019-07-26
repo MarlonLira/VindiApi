5842a64a49339e8e79ece843a8419d767f904ab9
Marlon Lira <marlonlira3@gmail.com>
2019-07-26 16:02:08 -0300

    modificações

--- Vindi/Program.cs
+++ Vindi/Program.cs
@@ -26,31 +26,6 @@ namespace Vindi
                 Console.WriteLine("id: " + RUser.id + " Name: " + RUser.name + " Status: " + RUser.base_role);
             }
 
-
-
-            /*
-            var plans = service.GetPlansByAnythingAsync().GetAwaiter().GetResult();
-            var plans2 = service2.GetByAnythingAsync(new Plan()).GetAwaiter().GetResult();
-            var products = service.GetProductsByAnythingAsync().GetAwaiter().GetResult();
-            var paymentMethods = service.GetPaymentMethoidsByAnythingAsync().GetAwaiter().GetResult();
-            var subscriptions = service.GetSubscriptionsByAnythingAsync().GetAwaiter().GetResult();
-            var bills = service.GetBillsByAnythingAsync().GetAwaiter().GetResult();
-            var charges = service.GetChargesByAnythingAsync().GetAwaiter().GetResult();
-            var invoices = service.GetInvoicesByAnythingAsync().GetAwaiter().GetResult();
-            var messages = service.GetMessagesByAnythingAsync().GetAwaiter().GetResult();
-            var importBatches = service.GetImportBatchesByAnythingAsync().GetAwaiter().GetResult();
-            var issues = service.GetIssuesByAnythingAsync().GetAwaiter().GetResult();
-            var notifications = service.GetNotificationsByAnythingAsync().GetAwaiter().GetResult();
-            var merchant_Users = service.GetMerchantUsersByAnythingAsync().GetAwaiter().GetResult();
-            var roles = service.GetRolesByAnythingAsync().GetAwaiter().GetResult();
-            /*Customer Customer = new Customer() {
-                id = 11054586,
-                name="juspencio silva"
-            };
-            
-
-           */
-
             Console.WriteLine("Hello World!");
         
         }
