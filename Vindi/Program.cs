9240a18171896453e4b6a080162c496eb8da2c60
Marlon Lira <marlonlira3@gmail.com>
2019-07-26 15:56:16 -0300

    correções nos metodos get

--- Vindi/Program.cs
+++ Vindi/Program.cs
@@ -7,18 +7,28 @@ namespace Vindi
     {
         static void Main(string[] args) {
 
-            var config = new VindiConfiguration("https://app.vindi.com.br", 1, "XlZBPa4zUhX1In4T9yHloj83WNaJf0i7V386V_Q2xQk");
-            var config2 = new Configuration("https://app.vindi.com.br", 1, "XlZBPa4zUhX1In4T9yHloj83WNaJf0i7V386V_Q2xQk");
+            var config = new Configuration("https://app.vindi.com.br", 1, "XlZBPa4zUhX1In4T9yHloj83WNaJf0i7V386V_Q2xQk");
 
-            var service = new VindiService(config);
-            var service2 = new Service(config2);
-            var customers = service.GetCustomersByAnythingAsync().GetAwaiter().GetResult();
-            var customers2 = service2.GetByAnythingAsync(new Customer()).GetAwaiter().GetResult();
-            List<Customer> Test = (List<Customer>)customers2;
+            var service = new Service(config);
+            var customers = service.GetByAnythingAsync(new Customer()).GetAwaiter().GetResult();
+            var merchant_Users = service.GetByAnythingAsync(new Merchant_Users()).GetAwaiter().GetResult();
+            var roles = service.GetByAnythingAsync(new Role()).GetAwaiter().GetResult();
+            List<Customer> Test = (List<Customer>)customers;
+            IList<Merchant_Users> Users = (IList<Merchant_Users>)merchant_Users;
+            IList<Role> Lroles = (IList<Role>)roles;
             foreach (Customer Entite in Test) {
                 Console.WriteLine("id: " + Entite.id + " Name: " + Entite.name + " Cpf: " + Entite.registry_code);
             }
-            Plan plan = new Plan();
+            foreach (Merchant_Users User in Users) {
+                Console.WriteLine("id: " + User.id + " Name: " + User.user + " Status: " + User.status);
+            }
+            foreach (Role RUser in Lroles) {
+                Console.WriteLine("id: " + RUser.id + " Name: " + RUser.name + " Status: " + RUser.base_role);
+            }
+
+
+
+            /*
             var plans = service.GetPlansByAnythingAsync().GetAwaiter().GetResult();
             var plans2 = service2.GetByAnythingAsync(new Plan()).GetAwaiter().GetResult();
             var products = service.GetProductsByAnythingAsync().GetAwaiter().GetResult();
@@ -37,9 +47,9 @@ namespace Vindi
                 id = 11054586,
                 name="juspencio silva"
             };
+            
 
-            //service.CreateCustomersAsync(Customer);
-            service.UpdateCustomersAsync(Customer.id, Customer);*/
+           */
 
             Console.WriteLine("Hello World!");
         
--- Vindi/Service.cs
+++ Vindi/Service.cs
@@ -51,7 +51,7 @@ namespace Vindi
         #region Get Methods
 
         //Retorna todos os produtos
-        public async Task<IEnumerable<Product>> GetProductsByAnythingAsync(Product Product ,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
+        public async Task<IEnumerable<Product>> GetByAnythingAsync(Product Product ,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("products", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Product>>(list?.products);
         }
@@ -75,7 +75,7 @@ namespace Vindi
         }
 
         //Retorna o plano pelo id informado
-        public async Task<Plan> GetPlansByIdAsync(Plan Plan ,Int32 Id) {
+        public async Task<Plan> GetByIdAnythingAsync(Plan Plan ,Int32 Id) {
             var result = await SearchByIdAsync("plans", Id);
             return FromDynamicTo<Plan>(result?.plan);
         }
@@ -93,7 +93,7 @@ namespace Vindi
         }
 
         //Retorna todos os cadastros
-        public async Task<IEnumerable<Subscription>> GetSubscriptionsByAnythingAsync(Subscription Subscription,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
+        public async Task<IEnumerable<Subscription>> GetByAnythingAsync(Subscription Subscription,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("subscriptions", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Subscription>>(list?.subscriptions);
         }
@@ -129,7 +129,7 @@ namespace Vindi
         }
 
         //Retorna todos os perfis de pagamento
-        public async Task<IEnumerable<Payment_Profile>> GetPaymentProfilesByAnythingAsync(IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
+        public async Task<IEnumerable<Payment_Profile>> GetByAnythingAsync( Payment_Profile PaymentProfile,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("payment_profiles", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Payment_Profile>>(list?.payment_profiles);
         }
@@ -140,31 +140,31 @@ namespace Vindi
             return FromDynamicTo<Payment_Profile>(result?.payment_profile);
         }
 
-        //Retorna todas as cargas
+        //Retorna todas as cobranças
         public async Task<IEnumerable<Charge>> GetByAnythingAsync(Charge Charge,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.created_at, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("charges", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Charge>>(list?.charges);
         }
 
-        //Retorna a carga pelo id informado
+        //Retorna a cobranças pelo id informado
         public async Task<Charge> GetByIdAnythingAsync(Charge Charge, Int32 Id) {
             var result = await SearchByIdAsync("charges", Id);
             return FromDynamicTo<Charge>(result?.charge);
         }
 
-        //Retorna todas as contas
+        //Retorna todas as faturas
         public async Task<IEnumerable<Bill>> GetByAnythingAsync(Bill Bill, IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("bills", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Bill>>(list?.bills);
         }
-        
-        //Retorna a conta pelo id informado
+
+        //Retorna a faturas pelo id informado
         public async Task<Bill> GetByIdAnythingAsync(Bill Bill, Int32 Id) {
             var result = await SearchByIdAsync("bills", Id);
             return FromDynamicTo<Bill>(result?.bill);
         }
 
-        //Retorna o item da conta pelo id informado
+        //Retorna o item da fatura pelo id do item informado
         public async Task<Bill_Items> GetByIdAnythingAsync(Bill_Items Bill_Items,Int32 Id) {
             var result = await SearchByIdAsync("bill_items", Id);
             return FromDynamicTo<Bill_Items>(result?.bill_item);
@@ -201,7 +201,7 @@ namespace Vindi
         }
 
         //
-        public async Task<IEnumerable<Issue>> GetByAnythingAsync(IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
+        public async Task<IEnumerable<Issue>> GetByAnythingAsync(Issue Issue,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("issues", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Issue>>(list?.issues);
         }
@@ -212,25 +212,25 @@ namespace Vindi
             return FromDynamicTo<Issue>(result?.issue);
         }
 
-        //
-        public async Task<IEnumerable<Merchant>> GetByAnythingAsync(Merchant Merchant ,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
+        //Retorna todas as empresas cadastradas
+        public async Task<IEnumerable<Merchant>> GetByAnythingAsync(Merchant Merchant, IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("merchants", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Merchant>>(list?.merchants);
         }
 
-        //
+        //Retorna a empresa cadastrada pelo id informado
         public async Task<Merchant> GetByIdAnythingAsync(Merchant Merchant, Int32 Id) {
             var result = await SearchByIdAsync("merchants", Id);
             return FromDynamicTo<Merchant>(result?.merchant);
         }
 
-        //
+        //Retorna todos os usuarios associados cadastrados.
         public async Task<IEnumerable<Merchant_Users>> GetByAnythingAsync(Merchant_Users MerchantUsers ,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("merchant_users", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Merchant_Users>>(list?.merchant_users);
         }
 
-        //
+        //Retorna os usuarios associados pelo id informado.
         public async Task<Merchant_Users> GetByIdAnythingAsync(Merchant_Users MerchantUsers ,Int32 Id) {
             var result = await SearchByIdAsync("merchant_users", Id);
             return FromDynamicTo<Merchant_Users>(result?.merchant_user);
@@ -259,13 +259,13 @@ namespace Vindi
             return FromDynamicTo<Notification>(result?.notification);
         }
 
-        //
-        public async Task<IEnumerable<Role>> GetByIdAnythingAsync(Role Role ,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
+        //Retorna todos os perfis de acesso
+        public async Task<IEnumerable<Role>> GetByAnythingAsync(Role Role ,IDictionary<FilterSearch, string> query = null, int page = 1, int perPage = 20, FilterSearch filterSearch = FilterSearch.id, SortOrder sortOrder = SortOrder.asc) {
             var list = await SearchByAnythingAsync("roles", query, page, perPage, filterSearch, sortOrder);
             return FromDynamicTo<IEnumerable<Role>>(list?.roles);
         }
 
-        //
+        //Retorna o perfil de acesso pelo id informado.
         public async Task<Role> GetByIdAnythingAsync(Role Role ,Int32 Id) {
             var result = await SearchByIdAsync("roles", Id);
             return FromDynamicTo<Role>(result?.role);
