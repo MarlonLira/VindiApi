dd1ffbea10250a17e6bf371010c20ab89c8a6126
MarlonLira <marlonlira3@gmail.com>
2019-08-03 11:35:55 -0300

    Criação da Classe Vindi e alguns ajustes
    Criação

---
+++ Vindi/Vindi.cs
@@ -0,0 +1,51 @@
+﻿namespace Vindi {
+    class Vindi {
+
+        #region Configs
+
+        private Configuration Config = new Configuration("https://app.vindi.com.br", 1, "XlZBPa4zUhX1In4T9yHloj83WNaJf0i7V386V_Q2xQk");
+        private Service Service;
+
+        #endregion
+
+        #region Methods
+
+        public dynamic GetByAnythingAsync(dynamic Entitie) {
+            Service = new Service(Config);
+            dynamic Result = "";
+
+            Result = Service.GetByAnythingAsync(Entitie).GetAwaiter().GetResult();
+
+            return Result;
+        }
+
+        public dynamic CreateAnythingAsync(dynamic Entitie) {
+            Service = new Service(Config);
+            dynamic Result = "";
+
+            Result = Service.CreateAnythingAsync(Entitie).GetAwaiter().GetResult();
+
+            return Result;
+        }
+
+        public dynamic UpdateAnythingAsync(dynamic Entitie) {
+            Service = new Service(Config);
+            dynamic Result = "";
+
+            Result = Service.UpdateAnythingAsync(Entitie).GetAwaiter().GetResult();
+
+            return Result;
+        }
+
+        public dynamic DeleteAnythingAsync(dynamic Entitie) {
+            Service = new Service(Config);
+            dynamic Result = "";
+
+            Result = Service.DeleteAnythingAsync(Entitie).GetAwaiter().GetResult();
+
+            return Result;
+        }
+
+        #endregion
+    }
+}
